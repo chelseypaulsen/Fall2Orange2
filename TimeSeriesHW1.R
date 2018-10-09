c44ad73c53b07517e62dd0db46e0cf47b35bdfac
acrusse5 <42445081+acrusse5@users.noreply.github.com>
2018-10-09 16:39:30 -0400

    added training/test split and plot

--- TimeSeriesHW1.R
+++ TimeSeriesHW1.R
@@ -47,57 +47,61 @@ well_ft <- read.zoo(df)
 # Impute missing values (fill in well_ft NAs)
 well_ft_impute <- na.approx(well_ft)
 
+#Split into training and test
+train_well = well_ft_impute[1:93623,]
+test_well = well_ft_impute[93624:93791,]
+
 #making time series plots
 plot(well_ft_impute,xlab = "Time (Years)", ylab = "Corrected Well Height (feet)", main="Time Series Plot of Well 3549 Height")
 
 #time series decomposition 
-decomp <- ts(well_ft_impute, freq=8760)
+decomp <- ts(train_well, freq=8760)
 new <- stl(decomp, s.window = 7)
 plot(new)
 
 #checking various seasonal components
 #checking yearly seasons  (check)
-decomp <- ts(well_ft_impute, freq=8766)
+decomp <- ts(train_well, freq=8766)
 new <- stl(decomp, s.window = 7)
 plot(new)
 #yearly seasons present
 
 #6months
-decomp2 <- ts(well_ft_impute, freq=4383)
+decomp2 <- ts(train_well, freq=4383)
 new <- stl(decomp2, s.window = 7)
 plot(new)
 #6 month seasons present
 
 #checking for monthly seasonal component (29.5 days-based on lunar calendar)
-well_mth <- ts(well_ft_impute[1:4248,]) #truncating to view more easily
+well_mth <- ts(train_well[1:4248,]) #truncating to view more easily
 decomp_mth <- ts(well_mth, freq=708)
 new_mth <- stl(decomp_mth, s.window=7)
 plot(new_mth)
 #monthly season present
 
 #checking for weekly seasonal component
-well_wk <- ts(well_ft_impute[1:1000,])
+well_wk <- ts(train_well[1:1000,])
 decomp_wk <- ts(well_wk, freq=168)
 new_wk <- stl(decomp_wk, s.window=7)
 plot(new_wk)
 #no weekly season
 
 #checking for daily seasonal component
-well_day <- ts(well_ft_impute[1:480,])
+well_day <- ts(train_well[1:480,])
 decomp_day <- ts(well_day, freq=24)
 new_day <- stl(decomp_day, s.window=7)
 plot(new_day)
 #no daily season 
 
 #checking lunar season
-well_lun <- ts(well_ft_impute[1:480,])
+well_lun <- ts(train_well[1:480,])
 decomp_lun <- ts(well_lun, freq=27.3)
 new_lun <- stl(decomp_lun, s.window=7)
 plot(new_lun)
 #no lunar season
 
 #checking tidal season
-well_tide  <- ts(well_ft_impute[1:480,])
+well_tide  <- ts(train_well[1:480,])
 decomp_tide <- ts(well_tide, freq=12.5)
 new_tide <- stl(decomp_tide, s.window=7)
 plot(new_tide)
@@ -116,7 +120,7 @@ daily = 24
 biannual = 24*(365.25/2)
 
 #trying with two seasons
-seasons <- msts(decomp, start=1, seasonal.periods = c(yearly, monthly, biannual))
+seasons <- msts(train_well, start=1, seasonal.periods = c(yearly, monthly, biannual))
 
 #fitting sine/cosine with fourier
 arima.3<-Arima(seasons,order=c(4,1,6), xreg=fourier(seasons,K=c(2,2,2)))
@@ -142,4 +146,15 @@ White.LB <- pmin(White.LB, 0.2)
 barplot(White.LB, main = "Ljung-Box Test P-values", ylab = "Probabilities", xlab = "Lags", ylim = c(0, 0.2))
 abline(h = 0.01, lty = "dashed", col = "black")
 abline(h = 0.05, lty = "dashed", col = "black")
+
+
+#plotting forecasted and test values for last week
+arima.4<-Arima(train_well,order=c(4,1,6))#, xreg=fourier(seasons,K=c(2,2,2)))
+summary(arima.4)
+f1=forecast(arima.4, h=168)#,xreg=fourier(test_seasons,K=c(2,2,2)))
+
+plot(test_well, col="red", ylim=c(.1,.6))
+lines(f1$mean)
+lines(f1$upper[,2])
+lines(f1$lower[,2])
 
