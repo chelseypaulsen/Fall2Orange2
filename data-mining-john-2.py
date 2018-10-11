089c218cb667de0ec09115843514485c5ee1d3ea
John Coletta <jbcolett@ncsu.edu>
2018-10-11 13:30:46 -0400

    Created and fit decision tree classifier

--- data-mining-john-2.py
+++ data-mining-john-2.py
@@ -7,10 +7,16 @@ Created on Thu Oct 11 13:06:24 2018
 
 import pandas as pd
 import os
+from sklearn import tree
 
-os.chdir('C:\Users\johnb\OneDrive\Documents\MSA\Fall 2\Data Mining\HW2')
+os.chdir('C:\\Users\\johnb\\OneDrive\\Documents\\MSA\\Fall 2\\Data Mining\\HW2')
 
 file = 'FFTSales.xls'
 
 sales = pd.read_excel(file)
+features = sales.drop('y', axis=1)
+target = sales['y']
+# Fit the classifier
+clf = tree.DecisionTreeClassifier()
 
+clf = clf.fit(features, target)
