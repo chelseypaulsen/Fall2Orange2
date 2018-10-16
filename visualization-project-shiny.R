3dc06ef1284389c6e7b018ae0526eb6fb909341e
John Coletta <jbcolett@ncsu.edu>
2018-10-16 10:40:22 -0400

    Used imputeTS to impute missing values into timeseries

--- visualization-project-shiny.R
+++ visualization-project-shiny.R
@@ -18,24 +18,25 @@ library(foreign)
 library(caschrono)
 library(TSA)
 library(quantmod)
+library(imputeTS)
 
 data.dir <- 'C:/Users/johnb/OneDrive/Documents/MSA/Fall 2/Well Data/'
 
-wells <- c('F-45','F-179','F-319','G-561_T','G-580A','G-852','G-860','G-1220_T',
+wells <- c('F-45','F-179','F-319','G-561_T','G-580A','G-860','G-1220_T',
            'G-1260_T','G-2147_T','G-2866_T','G-3549','PB-1680_T')
 
 start <- ymd_h("2007/10/01 00", tz='UTC')
 end <- ymd_h("2018/06/12 23", tz='UTC')
-
+startday <- as.numeric(strftime(start, format='%j'))
 hourseq <- seq(start,end, by='hours')
 
 full_df <- data.frame(datetime=hourseq)
 
 for (well in wells){
-  if (well == 'G-582'){
-    df <- read.xlsx(paste(data.dir,well,'.xlsx',sep=''),sheet='Well',
-                    colNames=c('date','time','tz_cd','well_ft','Code',
-                               'Corrected',startRow=33))
+  if (well == 'G-852'){
+    df <- read_excel(paste(data.dir,well,'.xlsx',sep=''),sheet='Well',
+                    col_names=c('date','time','tz_cd','well_ft','Code',
+                               'Corrected'),skip=32)
     well_df <- data.frame(df)
     head(well_df)
     well_df_clean <- mutate(well_df, time=hour(time))  # adds date to datetime
@@ -46,10 +47,14 @@ for (well in wells){
       filter(datetime >= ymd_hm('2007-10-01 01:00') &    # filters to dates defined in Simmons instructions
                datetime <= ymd_hm('2018-06-12 23:00'))
     well_df_clean <- well_df_clean %>% select(datetime,well_ft)
-    names(well_df_clean)[names(well_df_clean) == 'well_ft'] <- paste('well_ft',well,sep='_')
+    timeseries <- ts(well_df_clean$well_ft, start=c(2007,startday*24), frequency=(365.25*24))
+    imputed <- na.seadec(timeseries, algorithm='locf')
+    well_df_clean$filled <- imputed
+    names(well_df_clean)[names(well_df_clean) == 'filled'] <- paste('filled',well,sep='_')
+    well_df_clean <- well_df_clean %>% select(filled, datetime)
     full_df <- full_df %>% left_join(well_df_clean, by='datetime')
   }
-  }
+  else{
   df <- read_xlsx(paste(data.dir,well,'.xlsx',sep=''),sheet='Well')
   well_df <- data.frame(df)
   head(well_df)
@@ -61,6 +66,16 @@ for (well in wells){
     filter(datetime >= ymd_hm('2007-10-01 01:00') &    # filters to dates defined in Simmons instructions
              datetime <= ymd_hm('2018-06-12 23:00'))
   well_df_clean <- well_df_clean %>% select(datetime,well_ft)
-  names(well_df_clean)[names(well_df_clean) == 'well_ft'] <- paste('well_ft',well,sep='_')
+  timeseries <- ts(well_df_clean$well_ft, start=c(2007,startday*24), frequency=(365.25*24))
+  imputed <- na.seadec(timeseries, algorithm='locf')
+  well_df_clean$filled <- imputed
+  well_df_clean <- well_df_clean %>% select(datetime, filled)
+  names(well_df_clean)[names(well_df_clean) == 'filled'] <- paste('filled',gsub('-','',well),sep='_')
   full_df <- full_df %>% left_join(well_df_clean, by='datetime')
-}
+  }}
+head(full_df)
+
+ggplot(full_df, aes(x=datetime,y=filled_G580A)) +
+  geom_line()
+
+names(full_df)
